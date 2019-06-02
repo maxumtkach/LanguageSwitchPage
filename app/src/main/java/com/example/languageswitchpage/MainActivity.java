6b1288184dd9035703f153555915a0abd4900269
maxumtkach <makstkach@bigmir.net>
2019-06-02 12:31:31 +0300

    //3.3.2 Страница переключения языка

---
+++ app/src/main/java/com/example/languageswitchpage/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.languageswitchpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void langeClick(View view) {
+
+            Locale locale = new Locale("en");
+            Configuration config = new Configuration();
+            config.setLocale(locale);
+            getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+            recreate();
+    }
+
+    public void langeRuClick(View view) {
+        Locale locale = new Locale("ru");
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+        getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        recreate();
+    }
+}
