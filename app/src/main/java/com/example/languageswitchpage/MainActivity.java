f58cd37b528419f429f12c888f76a31a406114f4
maxumtkach <makstkach@bigmir.net>
2019-06-03 22:01:34 +0300

    //3.3.2 Задача 2. Страница переключения языка.

--- app/src/main/java/com/example/languageswitchpage/MainActivity.java
+++ app/src/main/java/com/example/languageswitchpage/MainActivity.java
@@ -5,31 +5,66 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
     }
 
-    public void langeClick(View view) {
-
-            Locale locale = new Locale("en");
-            Configuration config = new Configuration();
-            config.setLocale(locale);
-            getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
-            recreate();
+    private void initViews() {
+        spinner = findViewById(R.id.spinner_lang);
+        spinner.setOnItemSelectedListener(itemSelectedListener);
+        initSpinnerLang();
     }
 
-    public void langeRuClick(View view) {
-        Locale locale = new Locale("ru");
-        Configuration config = new Configuration();
-        config.setLocale(locale);
-        getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
-        recreate();
+    private void initSpinnerLang() {
+
+        ArrayAdapter<CharSequence> adapterCountries = ArrayAdapter.createFromResource(this, R.array.language, android.R.layout.simple_spinner_item);
+        adapterCountries.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapterCountries);
     }
+
+    AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            // Получаем выбранный объект
+            String item = (String) parent.getItemAtPosition(position);
+
+            if (item.equals("английский")) {
+                Locale locale = new Locale("en");
+                Configuration config = new Configuration();
+                config.setLocale(locale);
+                getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+                recreate();
+            }
+            if (item.equals("russian")) {
+                Locale locale = new Locale("ru");
+                Configuration config = new Configuration();
+                config.setLocale(locale);
+                getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+                recreate();
+            }
+
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
 }
+
