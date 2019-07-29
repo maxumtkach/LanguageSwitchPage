0269a172b91c2043d799232d2055f982a0843254
maxumtkach <makstkach@bigmir.net>
2019-07-29 08:32:41 +0300

    //5/2/2

--- app/src/main/java/com/example/languageswitchpage/MainActivity.java
+++ app/src/main/java/com/example/languageswitchpage/MainActivity.java
@@ -34,11 +34,12 @@ public class MainActivity extends AppCompatActivity {
     private static final String IS_CHECK = " is checked";
     private static final String PREFS_FILE = "Account";
 
+    private static final String EXTERNAL_DIRECTORY_NAME = "external_directory";
+    private static final String EXTERNAL_LOGIN_FILE_NAME = "external_login.txt";
+    private static final String EXTERNAL_PASS_FILE_NAME = "external_pass.txt";
+
     private static final String LOGIN_FILE_NAME = "login text";
     private static final String PASS_FILE_NAME = "pass text";
-    private String filePath = "MyFileStorage";
-    private String fileName = "mFile.txt";
-    private File mFile;
 
     private EditText loginTextView;
     private EditText passwordTextView;
@@ -55,24 +56,11 @@ public class MainActivity extends AppCompatActivity {
 
         initSpinnerLang();
         setCheckChange();
-        saveSharedPreferences();
-        checkBol();
-    }
-
-    private void saveSharedPreferences() {
-
         sharedPreferences = getSharedPreferences(PREFS_FILE, MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putBoolean(IS_CHECK, true);
-        editor.apply();
+        checkBox.setChecked(sharedPreferences.getBoolean(IS_CHECK, true));
     }
 
-    private void checkBol() {
-
-        boolean booleanCheck = sharedPreferences.getBoolean(IS_CHECK, true);
-        checkBox.setChecked(booleanCheck);
-    }
-
+    //сохраняем значение чека
     public void setCheckChange() {
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -81,11 +69,20 @@ public class MainActivity extends AppCompatActivity {
                         .edit()
                         .putBoolean(IS_CHECK, isChecked)
                         .apply();
+                updateButtons();
             }
         });
     }
 
+    //  условие активности кнопок
+    private void updateButtons() {
+        if (checkBox.isChecked() && (!isAvailable() || isReadOnly())) {
+            registrationButton.setEnabled(false);
+            loginButton.setEnabled(false);
+        }
+    }
 
+    // проверяем есть ли доступ к внешнему хранилищу
     private static boolean isReadOnly() {      // проверяем есть ли доступ к внешнему хранилищу
         String storageState = Environment.getExternalStorageState();
         return Environment.MEDIA_MOUNTED_READ_ONLY.equals(storageState);
@@ -97,7 +94,8 @@ public class MainActivity extends AppCompatActivity {
         return Environment.MEDIA_MOUNTED.equals(storageState);
     }
 
-    private static boolean textUtilsValue(String str1, String str2) {  // условие не пустых полей строк
+    // условие не пустых полей строк
+    private static boolean textUtilsValue(String str1, String str2) {
 
         if (TextUtils.isEmpty(str1) || TextUtils.isEmpty(str2)) {
             return true;
@@ -110,10 +108,12 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
-    private String readLineFromFileExt(File file) {   //read   внешний
+    private String readLineFromFileExt(String fileName) {   //read   внешний
         FileInputStream fis = null;
         try {
-            fis = new FileInputStream(file);
+            fis = new FileInputStream(new File(getExternalFilesDir(EXTERNAL_DIRECTORY_NAME), fileName));
+
+            // fis = new FileInputStream(file);
             DataInputStream in = new DataInputStream(fis);
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             return br.readLine();
@@ -154,10 +154,11 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void saveExtData(String editText) {    //сохр во внешний файл
+    private void saveExtData(String fileName, String editText) {    //сохр во внешний файл
         FileOutputStream fos = null;
         try {
-            fos = new FileOutputStream(mFile);
+            fos = new FileOutputStream(new File(getExternalFilesDir(EXTERNAL_DIRECTORY_NAME), fileName));
+            //    fos = new FileOutputStream(mFile);
             fos.write(editText.getBytes());
             fos.close();
         } catch (IOException e) {
@@ -250,18 +251,9 @@ public class MainActivity extends AppCompatActivity {
                 saveIntData(PASS_FILE_NAME, passwordStr);
                 showToast(getString(R.string.saving_to_internal_storage));
             } else {
-
-                if (!isAvailable() || isReadOnly()) {
-
-                    registrationButton.setEnabled(false);
-                    loginButton.setEnabled(false);
-                } else {
-                    // если доступ есть, то создаем файл в ExternalStorage
-                    mFile = new File(getExternalFilesDir(filePath), fileName);
-                }
-                saveExtData(loginStr + passwordStr);
+                saveExtData(EXTERNAL_LOGIN_FILE_NAME, loginStr);
+                saveExtData(EXTERNAL_PASS_FILE_NAME, passwordStr);
                 showToast(getString(R.string.saving_to_external_storage));
-
             }
         }
     }
@@ -272,33 +264,24 @@ public class MainActivity extends AppCompatActivity {
         if (textUtilsValue(loginStr, passwordStr)) {
             showToast(getString(R.string.nou_pass_nou_login));
         } else {
+            String savedLogin;
+            String savedPassword;
             if (checkBox.isChecked()) {
-                String savedLogin = readLineFromFile(LOGIN_FILE_NAME);
-                String savedPassword = readLineFromFile(PASS_FILE_NAME);
-                final boolean loginEquals = loginTextView.getText().toString().equals(savedLogin);
-                final boolean passwordEquals = passwordTextView.getText().toString().equals(savedPassword);
-                if (loginEquals && passwordEquals) {
-
-                    Toast.makeText(MainActivity.this, (R.string.access), Toast.LENGTH_LONG).show();
-
-                } else {
-                    Toast.makeText(MainActivity.this, (R.string.no_access), Toast.LENGTH_LONG).show();
-
-                }
-
+                savedLogin = readLineFromFile(LOGIN_FILE_NAME);
+                savedPassword = readLineFromFile(PASS_FILE_NAME);
             } else {
-                String massage = readLineFromFileExt(mFile);
+                savedLogin = readLineFromFileExt(EXTERNAL_LOGIN_FILE_NAME);
+                savedPassword = readLineFromFileExt(EXTERNAL_PASS_FILE_NAME);
                 showToast(getString(R.string.data_obtained_from_external_memory));
-                final boolean loginEquals = (loginTextView.getText().toString()
-                        + passwordTextView.getText().toString()).equals(massage);
+            }
+            final boolean loginEquals = loginTextView.getText().toString().equals(savedLogin);
+            final boolean passwordEquals = passwordTextView.getText().toString().equals(savedPassword);
+            if (loginEquals && passwordEquals) {
 
-                if (loginEquals) {
-                    showToast(getString(R.string.access));
-                } else {
-                    showToast(getString(R.string.no_access));
-                }
+                Toast.makeText(MainActivity.this, (R.string.access), Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(MainActivity.this, (R.string.no_access), Toast.LENGTH_LONG).show();
             }
         }
     }
 }
-
